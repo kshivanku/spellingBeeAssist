2a1611454a98b8142bd3c4d142c39a9c73561efa
Kumar <shivankumar@lm-sjn-21010683.paypalcorp.com>
2017-06-06 13:03:45 -0700

    testing if postrequest is running

--- index.js
+++ index.js
@@ -5,7 +5,20 @@ const ApiAiApp = require('actions-on-google').ApiAiApp;
 const restService = express();
 
 restService.post('/hook', function(req, res){
+  console.log("request: ");
   console.log(req);
+  const app = new ApiAiApp({
+    request: req,
+    response: res
+  });
+
+  function gameAction(app){
+    app.tell("this is a response from the webhook");
+  }
+
+  const actionMap = new Map();
+  actionMap.set('game.action', gameAction);
+  app.handleRequest(actionMap);
 });
 
 restService.listen((process.env.PORT || 5000), function() {
