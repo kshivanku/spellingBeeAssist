efc65838ef6dbe8e2268b6fcd344278721d06fd4
Kumar <shivankumar@lm-sjn-21010683.paypalcorp.com>
2017-06-06 14:28:08 -0700

    small changes

--- index.js
+++ index.js
@@ -6,24 +6,27 @@ const request = require('request');
 const restService = express();
 restService.use(bodyParser.json());
 
-restService.post('/hook', function(req, res){
+restService.post('/hook', function(req, res) {
   const app = new ApiAiApp({
     request: req,
     response: res
   });
 
-  function welcomeUser(app){
+  function welcomeUser(app) {
     app.setContext("wakeUpGame", 2);
     app.ask("Hi! this is spelling bee, do you want to play?", ['do you want to play?', 'Say yes if you want to play', 'We can stop here. See you soon!']);
   }
 
-  function gameAction(app){
+  function gameAction(app) {
     var userResponse = app.getArgument("confirm_command");
-    if(userResponse == "positive") {
-      var word = generateRandomWord();
+    if (userResponse == "positive") {
+      var word = "";
+      do {
+        word = generateRandomWord();
+      }
+      while (/\s/.test(word));
       app.tell("Awesome! Lets start. Spell the word " + word);
-    }
-    else {
+    } else {
       app.tell("Too bad, see you later!");
     }
   }
@@ -34,22 +37,13 @@ restService.post('/hook', function(req, res){
   app.handleRequest(actionMap);
 });
 
-function generateRandomWord(){
-  var continueLoop = true;
-  while(continueLoop){
-    request.get({
-      url: "http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=-1&maxDictionaryCount=-1&minLength=15&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"
-    }, function (err, response, body){
-      body = JSON.parse(body);
-      if(/\s/.test(word)) { //checking if wordnick returned a single word or a sentence
-        continueLoop = true;
-      }
-      else{
-        continueLoop = false
-        return body.word;
-      }
-    });
-  }
+function generateRandomWord() {
+  request.get({
+    url: "http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=-1&maxDictionaryCount=-1&minLength=15&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"
+  }, function(err, response, body) {
+    body = JSON.parse(body);
+    return body.word;
+  });
 }
 
 restService.listen((process.env.PORT || 5000), function() {
