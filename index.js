52041400bb7ab9e3b53fd4ae61ae61a2f7d8e1dc
Kumar <shivankumar@lm-sjn-21010683.paypalcorp.com>
2017-06-06 18:29:28 -0700

    small changes

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ restService.post('/hook', function(req, res) {
       var parameters = {}
       parameters["word"] = word;
       app.setContext("wordgiven", 10, parameters);
-      app.tell("Awesome! Lets start. Spell the word " + word + ". You can ask me the meaning of the word or usage in a sentence. Say, I am ready to spell, whenever you are ready to give your answer.");
+      app.tell("Awesome! Lets start. Spell the word " + word + ". You can ask me the meaning of the word or usage in a sentence. Say 'I am ready to spell', whenever you are ready to give your answer.");
     });
   }
 
@@ -62,7 +62,7 @@ restService.post('/hook', function(req, res) {
       body = JSON.parse(body);
       console.log(body);
       var definition = body[0].text;
-      app.tell("The meanings of the word is. " + definition + ". Say, I am ready to spell, whenever you are ready to give your answer.");
+      app.tell("The meanings of the word is. " + definition + ". Say 'I am ready to spell', whenever you are ready to give your answer.");
     });
   }
 
@@ -77,13 +77,13 @@ restService.post('/hook', function(req, res) {
       console.log("response body: ");
       console.log(body);
       console.log("usage: " + usage);
-      app.tell("Here's how you can use it in a sentence. " + usage + ". Say, I am ready to spell, whenever you are ready to give your answer.");
+      app.tell("Here's how you can use it in a sentence. " + usage + ". Say 'I am ready to spell', whenever you are ready to give your answer.");
     });
   }
 
   function repeatWord(app){
     var word = app.getContextArgument("wordgiven", "word");
-    app.tell("The word is, " + word.value + ". Say, I am ready to spell, whenever you are ready to give your answer.");
+    app.tell("The word is, " + word.value + ". Say 'I am ready to spell', whenever you are ready to give your answer.");
   }
 
   function readytospell(app){
