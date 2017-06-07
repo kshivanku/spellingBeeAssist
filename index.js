19af79305aa5a6a1db5af48f84a2547a1d0fbc98
Kumar <shivankumar@lm-sjn-21010683.paypalcorp.com>
2017-06-06 18:34:24 -0700

    small changes

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ restService.post('/hook', function(req, res) {
       var parameters = {}
       parameters["word"] = word;
       app.setContext("wordgiven", 10, parameters);
-      app.tell("Awesome! Lets start. Spell the word, '" + word + "'. \nYou can ask me the meaning of the word or usage in a sentence. Say 'I am ready to spell', whenever you are ready to give your answer.");
+      app.tell("Awesome! Spell the word, '" + word + "'. \n Please note, you can ask me the meaning of the word or usage in a sentence. Say 'I am ready to spell', whenever you are ready to give your answer. Your word is, '" + word + "'.");
     });
   }
 
